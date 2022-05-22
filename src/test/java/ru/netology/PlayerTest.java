74473d8b12ea871ce584ba9ff904f635bfa831ba
Ilkhom Dodov <dodlordisback@gmail.com>
2022-05-22 11:22:23 +0300

    Initial commit

--- src/test/java/ru/netology/PlayerTest.java
+++ src/test/java/ru/netology/PlayerTest.java
@@ -20,4 +20,129 @@ public class PlayerTest {
     }
 
     // другие ваши тесты
+
+    @Test
+    public void shouldThrowExceptionIfPlayGameThatIsNotInstalled() {
+        GameStore store = new GameStore();
+        Player player = new Player("Игрок");
+
+
+        Game game1 = new Game("Max Payne 1", "TPS", store);
+        Game game2 = new Game("Max Payne 2", "TPS", store);
+        Game game3 = new Game("Max Payne 3", "TPS", store);
+
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+
+        player.play(game1, 100);
+
+        assertThrows(RuntimeException.class, () -> {
+            player.play(game1, 100);
+        });
+    }
+
+    @Test
+    public void shouldSumGenreIfMultipleGames() {
+        GameStore store = new GameStore();
+        Player player = new Player("Игрок");
+
+        Game game1 = new Game("Max Payne 1", "TPS", store);
+        Game game2 = new Game("Max Payne 2", "TPS", store);
+        Game game3 = new Game("Max Payne 3", "TPS", store);
+
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+
+        player.play(game1, 500);
+        player.play(game2, 100);
+        player.play(game3, 100);
+
+        int expected = 700;
+        int actual = player.sumGenre("TPS");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSumHoursPlayedInOneGame() {
+        GameStore store = new GameStore();
+        Player player = new Player("Игрок");
+
+        Game game1 = new Game("Max Payne 1", "TPS", store);
+
+        player.installGame(game1);
+
+        player.play(game1, 25);
+        player.play(game1, 25);
+
+        int expected = 50;
+        int actual = player.sumGenre("TPS");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldSumGenresIfMoreThanOneGenre() {
+        GameStore store = new GameStore();
+        Player player = new Player("Игрок");
+
+        Game game1 = new Game("Max Payne 1", "TPS", store);
+        Game game2 = new Game("DOTA", "MOBA", store);
+        Game game3 = new Game("Counter-Strike", "FPS", store);
+
+        player.installGame(game1);
+        player.installGame(game2);
+        player.installGame(game3);
+
+        player.play(game1, 50);
+        player.play(game2, 50);
+        player.play(game3, 60);
+
+        int expected = 50;
+        int actual = player.sumGenre("TPS");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnMostPlayedGameByGenre() {
+        GameStore store = new GameStore();
+        Player player = new Player("Игрок");
+
+        Game game1 = new Game("NFS Underground", "Racing", store);
+        Game game2 = new Game("Forza", "Racing", store);
+
+        player.installGame(game1);
+        player.installGame(game2);
+
+        player.play(game1, 15);
+        player.play(game2, 20);
+
+        Game expected = game2;
+        Game actual = player.mostPlayerByGenre("Racing");
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldReturnNullIfYouSearchForGenreThatHasNotBeenPlayed() {
+        GameStore store = new GameStore();
+        Player player = new Player("Игрок");
+
+        Game game1 = new Game("NFS Underground", "Racing", store);
+        Game game2 = new Game("Forza", "Racing", store);
+
+        player.installGame(game1);
+        player.installGame(game2);
+
+        player.play(game1, 15);
+        player.play(game2, 20);
+
+        Game expected = null;
+        Game actual = player.mostPlayerByGenre("Fighting");
+
+        assertEquals(expected, actual);
+    }
 }
